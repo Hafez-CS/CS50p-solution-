95024d819e719509a22efa3f403c7acf5dde2da6
Mohammad Hafez Najafi <151014739+Hafez-CS@users.noreply.github.com>
2023-12-06 10:10:32 +0330

    Delete problem set 1  directory

--- problem set 1 /indoor voice/indoor.py
+++
@@ -1,2 +0,0 @@
-a = input()  # get some thing of user
-print(a.lower())  # lower the inpute and print it.
--- problem set 1 /making faces /faces.py
+++
@@ -1,3 +0,0 @@
-a = input()  # get some things of user
-b = a.replace(":)", "🙂")  # replace the small
-print(b.replace(":(", "🙁"))  # replace the sad
