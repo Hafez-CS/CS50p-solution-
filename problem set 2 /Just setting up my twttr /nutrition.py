c94983a17bdcabdecd6f12bb63d636cc48e7ab93
Mohammad Hafez Najafi <151014739+Hafez-CS@users.noreply.github.com>
2023-12-07 10:52:36 +0330

    Create nutrition.py

---
+++ problem set 2 /Just setting up my twttr /nutrition.py
@@ -0,0 +1,46 @@
+n = input("Item: ")
+
+if n == "Sweet Cherries":
+    print("Calories: 100")
+
+n = n.lower()
+
+
+if n == "apple":
+    print("Calories: 130")
+if n == "avocado":
+    print("Calories: 50")
+if n == "banana":
+    print("Calories: 110")
+if n == "cantaloupe":
+    print("Calories: 50")
+if n == "grapefruit":
+    print("Calories: 60")
+if n == "grapes":
+    print("Calories: 90")
+if n == "honeydew Melon":
+    print("Calories: 0")
+if n == "kiwifruit":
+    print("Calories: 90")
+if n == "lemon":
+    print("Calories: 15")
+if n == "lime":
+    print("Calories: 20")
+if n == "nectarine":
+    print("Calories: 60")
+if n == "orange":
+    print("Calories: 80")
+if n == "peach":
+    print("Calories: 60")
+if n == "pear":
+    print("Calories: 100")
+if n == "pineapple":
+    print("Calories: 50")
+if n == "plums":
+    print("Calories: 70")
+if n == "strawberries":
+    print("Calories: 50")
+if n == "tangerine":
+    print("Calories: 50")
+if n == "watermelon":
+    print("Calories: 80")
