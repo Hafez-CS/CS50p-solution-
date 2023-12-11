7b65dbac4d9cb77fd2e2585409f42a2edf2afe95
Mohammad Hafez Najafi <151014739+Hafez-CS@users.noreply.github.com>
2023-12-11 12:42:23 +0330

    Add files via upload

---
+++ problem set 5 /Back to the Bank/bank.py
@@ -0,0 +1,26 @@
+def main():
+    n = (input("Greeting: ")).lower() # get inpute of user with lower word
+    print(f"${value(n)}") # print it
+
+
+
+
+
+
+def value(a):
+    if a.startswith("hello"): # check it
+        return 0
+    elif a.startswith("h"): # check it
+        return 20
+    else:
+        return 100
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
